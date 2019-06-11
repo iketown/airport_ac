635741de3286df8900f3905462d75881303c1a71
ike76 <ike76@me.com>
2019-06-11 15:00:14 -0500

    remove unused node_modules

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express()
 require("dotenv").config()
 
 app.get("/country/:countryName/:keyword", (req, res) => {
-  console.log("req", req.params)
   const { countryName, keyword } = req.params
   let obj
   fs.readFile(`./airportCountries/${countryName}`, "utf8", function(err, data) {
@@ -17,23 +16,22 @@ app.get("/country/:countryName/:keyword", (req, res) => {
       if (ap.city && ap.city.toLowerCase().includes(searchString)) return true
       if (ap.state && ap.state.toLowerCase().includes(searchString)) return true
     })
-    // console.log("filtered", filteredResults)
+
     filteredResults.sort((a, b) => {
-      const aa = Number(a.carriers)
-      const bb = Number(b.carriers)
-      if (aa > bb) {
+      const aCarriers = Number(a.carriers)
+      const bCarriers = Number(b.carriers)
+      if (aCarriers > bCarriers) {
         return -1
       }
-      if (aa < bb) {
+      if (aCarriers < bCarriers) {
         return 1
       }
-      if (aa === bb) {
+      if (aCarriers === bCarriers) {
         if (Number(a.direct_flights) > Number(b.direct_flights)) return -1
         if (Number(a.direct_flights) < Number(b.direct_flights)) return 1
         return 0
       }
     })
-    // console.log("filtered after sort", filteredResults)
     res.json(filteredResults.slice(0, 10))
   })
 })
